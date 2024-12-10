9e1f1e5830000f9b1e48336a8359d23ff3241b10
rndltx <zaldydi17@gmail.com>
2024-12-11 01:29:12 +0800

    ftp

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'brs.rizsign.my.id',
+        pathname: '/uploads/**',
+      }
+    ],
+    domains: ['brs.rizsign.my.id']
   }
-  
+}
+
+module.exports = nextConfig
