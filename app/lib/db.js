b83206356f041298a5f452d8e7ac71cd287bbe96
Khairun Rizaldy <zaldydi17@gmail.com>
2024-12-10 20:23:01 +0700

    Update db.js

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,11 +1,13 @@
-// app/lib/db.ts
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'bub',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
