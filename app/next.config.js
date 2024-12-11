a6d9a312749c3675265db18fd5b37c3a24798ebe
Rndltx <zaldydi17@gmail.com>
2024-12-11 14:07:32 +0700

    ftp test

--- app/next.config.js
+++ app/next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'rizsign.my.id',
-        pathname: '/uploads/**',
-      }
-    ],
-    domains: ['rizsign.my.id']
-  }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
